81366c98e26f4e92a82f7902f9d7ba059fdf53f7
LacTuanMinh <jumming456@gmail.com>
2021-02-02 17:50:03 +0700

    fix

--- src/main/java/coaching/csv/DefaultCsvParser.java
+++ src/main/java/coaching/csv/DefaultCsvParser.java
@@ -9,83 +9,82 @@ import java.util.regex.Pattern;
  */
 public class DefaultCsvParser implements CsvParser {
 
-	Scanner scanner = null;
-	CsvFileConfig fileConfig = null;
-	int nSegment = 0;
+    Scanner scanner = null;
+    CsvFileConfig fileConfig = null;
+    int nSegment = 0;
 
-	/**
-	 * Initialize parser
-	 *
-	 * @param file         CSV file
-	 * @param parserConfig Configuration
-	 */
-	public DefaultCsvParser(File file, CsvFileConfig parserConfig) throws IOException {
+    /**
+     * Initialize parser
+     *
+     * @param file         CSV file
+     * @param parserConfig Configuration
+     */
+    public DefaultCsvParser(File file, CsvFileConfig parserConfig) throws IOException {
 
-		if (file == null || parserConfig == null) {
-			throw new IllegalArgumentException("Arguments can not be null");
-		}
+        if (file == null || parserConfig == null) {
+            throw new IllegalArgumentException("Arguments can not be null");
+        }
 
-		this.fileConfig = parserConfig;
+        this.fileConfig = parserConfig;
 
-		Scanner tempScanner = new Scanner(new FileReader(file));
+        Scanner tempScanner = new Scanner(new FileReader(file));
 
-		if (tempScanner.hasNext()) { // read first line and close it
-			String line = tempScanner.next();
-			this.nSegment = line.split(Pattern.quote(parserConfig.delimiter)).length;
-		}
+        if (tempScanner.hasNext()) { // read first line and close it
+            String line = tempScanner.next();
+            this.nSegment = line.split(Pattern.quote(parserConfig.delimiter)).length;
+        }
 
-		tempScanner.close();
+        tempScanner.close();
 
-		this.scanner = new Scanner(new FileReader(file));
-	}
+        this.scanner = new Scanner(new FileReader(file));
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void close() {
-		scanner.close();
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void close() {
+        scanner.close();
+    }
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @return
-	 */
-	public boolean hasNext() {
-		return scanner.hasNext();
-	}
+    /**
+     * {@inheritDoc}
+     *
+     * @return
+     */
+    public boolean hasNext() {
+        return scanner.hasNext();
+    }
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @return
-	 */
-	public CsvLine next() throws IOException {
+    /**
+     * {@inheritDoc}
+     *
+     * @return
+     */
+    public CsvLine next() throws IOException {
 
-		CsvLine csvLine = new CsvLine();
+        CsvLine csvLine = new CsvLine();
 
-		if (scanner.hasNext()) {
-			String line = scanner.next();
-			String[] segments;
+        if (scanner.hasNext()) {
+            String line = scanner.next();
+            String[] segments;
 
-			if (fileConfig.quoted) {
-				line = line.replaceAll("\"", "");
-			}
+            if (fileConfig.quoted) {
+                line = line.replaceAll("\"", "");
+            }
 
-			segments = line.split(Pattern.quote(fileConfig.delimiter), 0);
+            segments = line.split(Pattern.quote(fileConfig.delimiter), 0);
 
-			if(segments.length != nSegment)
-			{
-				this.close();
-				throw new IOException("Incompatible of data column.");
-			}
+            if (segments.length != nSegment) {
+                this.close();
+                throw new IOException("Incompatible of data column.");
+            }
 
-			for (int i = 0; i < segments.length; i++) {
-				csvLine.set(i, segments[i].trim());
-			}
-		}
+            for (int i = 0; i < segments.length; i++) {
+                csvLine.set(i, segments[i].trim());
+            }
+        }
 
-		return csvLine;
-	}
+        return csvLine;
+    }
 }
